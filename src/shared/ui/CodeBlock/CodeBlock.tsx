3e4fc591fed4ae32337cb8f10968053e7acfb4b3
LoremIps <master.htmlcss@bk.ru>
2026-08-20 23:13:29 +0600

    fix styles codeblock

--- src/shared/ui/CodeBlock/CodeBlock.tsx
+++ src/shared/ui/CodeBlock/CodeBlock.tsx
@@ -43,7 +43,36 @@ export function CodeBlock({ children }: CodeBlockProps) {
         onClick={handleCopy}
         aria-label="Скопировать код"
       >
-        {copied ? '✓' : '⧉'}
+        {copied ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <polyline points="20 6 9 17 4 12"></polyline>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+            <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+          </svg>
+        )}
       </button>
     </div>
   );
